099d050492c104d63e8f426fdf8d9bb385556a9c
MichalPigon <michaal.pigon@gmail.com>
2024-11-13 15:08:00 +0100

    geet

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,19 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
+    unsigned int suma = 0;
+    unsigned int count = 0;
+    Stocznia stocznia{};
+    while(suma<towar)
+        {
+            Statek *s = stocznia();
+            if(dynamic_cast<Zaglowiec*>(s))
+            {
+                count++;
+            }
+            suma = suma + s->transportuj();
+
+        }
     // Twoja implementacja tutaj
-    return 0;
+    return count;
 }
